5e5eaa821e0f66d4c38ccb977878a23cfcdb8e20
Julian Rüth <julian.rueth@fsfe.org>
2019-01-22 17:55:31 +0100

    Move global flags out of main.cc
    to make the library functional without main.cc

---
+++ src/libpolygon/globals.h
@@ -0,0 +1,94 @@
+#ifndef LIBPOLYGON_GLOBALS_H
+#define LIBPOLYGON_GLOBALS_H
+
+/*
+ * This file contains global variables that are set by the command line
+ * interface. Eventually this file should be empty. There should be no
+ * global state.
+ */ 
+
+/* command line options */
+
+extern int start_vertex;
+extern int end_vertex;
+extern bool perturb_euclidean;
+extern bool perturb_all;
+extern bool perturb_conjugate; 
+extern COORD depth;
+extern int perturb_vertex;
+extern Point offset;
+extern COORD DELTA;
+extern COORD THRESHOLD; 
+extern int mc_number;
+extern bool new_sweep;
+extern int mc_group;
+extern int max_priority;
+extern COORD follow_depth;
+extern int verbose;
+extern bool quiet;
+extern bool retriangulate;
+extern bool norescale;
+extern bool individual;
+extern bool norescale; 
+extern int random_seed; 
+extern int nice_inc; 
+extern int n_slices; 
+extern bool show_lengths;
+extern bool show_cyls;
+extern bool heights_and_twists;
+extern bool closure;
+extern bool allow_long_cyls;
+extern bool cyls_only;
+extern bool show_moduli;
+extern bool no_field_arithmetic;
+extern bool tikz_output; 
+extern bool show_length_list;
+extern bool draw_cylinders;
+extern bool draw_saddles;
+extern int draw_tag;
+extern COORD draw_saddle_length;
+extern bool billiard_mode;
+
+
+/* global variables */
+extern bool field_arithmetic;
+extern bool int_field_arithmetic;
+extern Summary smry;
+extern TwoComplex* S;
+extern int random_seed;
+extern FILE* out_f; 
+extern ostdiostream out_stream; 
+extern ostdiostream out_s; 
+extern bool too_close_flag;
+extern bool draw_cylinders;
+
+
+extern char filename[];  
+extern COORD perturb_magnitude; 
+
+/* to get around compiler changes */
+/* setting iterators to NULL no longer allowed, so have to do this */
+extern  list<OEdge>      stupid_OEdge_list;
+extern  list<OEdgeIter>  stupid_OEdgePtr_list;
+extern  list<VertexPtr>  stupid_VertexPtr_list;
+extern  list<UEdgePtr>   stupid_UEdgePtr_list;
+extern  list<FacePtr>    stupid_FacePtr_list;
+
+extern OEdgeIter NULL_OEdgeIter;
+extern OEdgePtrIter NULL_OEdgePtrIter; 
+extern VertexPtrIter NULL_VertexPtrIter;
+extern UEdgePtrIter NULL_UEdgePtrIter;
+extern FacePtrIter NULL_FacePtrIter;
+
+#ifdef USE_PARALLEL
+extern int numprocs;
+extern int my_id;
+#endif
+
+#ifdef USE_MPI
+extern void my_mpi_init(); 
+extern MPI_Datatype mpi_smry_type; 
+extern MPI_Op mpi_merge; 
+#endif
+
+#endif // LIBPOLYGON_GLOBALS_H
--- src/main.cc
+++ src/main.cc
@@ -10,87 +10,9 @@
 #include "slice-pvm.h"
 #endif
 
-int start_vertex = UNDEFINED;
-int end_vertex = UNDEFINED;
-bool perturb_euclidean = true;
-bool perturb_all = false;
-COORD perturb_magnitude = 0.0;
-bool perturb_conjugate = false; 
-COORD depth = 0.0;
-int perturb_vertex = UNDEFINED;
-Point offset = Point(UNDEFINED,UNDEFINED);
-int verbose = 0;
-COORD DELTA = 1E-9;
-COORD EPSILON = 1E-13;
 bool randomize = false;
-char filename[1000];
-bool file_arg  = false;
-int mc_number = 0;
-bool new_sweep = true;
-int mc_group = 100;
-int max_priority = 10;
-COORD follow_depth = -1000.0;
-bool quiet = false;
-bool retriangulate = true;
-bool norescale = false; 
-bool individual = false; 
-bool show_lengths = false; 
-bool show_cyls = false;
-bool cyls_only = false;
-bool allow_long_cyls = false;
-bool heights_and_twists = false;
-bool show_moduli = false;
-int nice_inc = 0; 
-int n_slices = 100;
-bool no_field_arithmetic = false; 
-bool closure = false;
-bool tikz_output = false;
-bool show_length_list = false; 
-bool draw_cylinders = false;
-bool draw_saddles = false;
-int draw_tag = -1;
-COORD draw_saddle_length = 0.0;
-bool billiard_mode = false; 
-
-/* global */
-bool field_arithmetic = false;
-bool int_field_arithmetic = false; 
-bool too_close_flag = false;
-//bool draw_cylinders = false; 
-
-TwoComplex *S; 
-int random_seed;
-FILE *out_f;
-
-ostdiostream out_stream; 
-
-
-/* more global vars to get around compiler changes */
-list<OEdge>      stupid_OEdge_list;
-list<OEdgeIter>  stupid_OEdgePtr_list;
-list<VertexPtr>  stupid_VertexPtr_list;
-list<UEdgePtr>   stupid_UEdgePtr_list;
-list<FacePtr>    stupid_FacePtr_list;
-
-
-
-
-
-OEdgeIter NULL_OEdgeIter = stupid_OEdge_list.end();
-OEdgePtrIter NULL_OEdgePtrIter = stupid_OEdgePtr_list.end();
-VertexPtrIter NULL_VertexPtrIter = stupid_VertexPtr_list.end();
-UEdgePtrIter NULL_UEdgePtrIter = stupid_UEdgePtr_list.end();
-FacePtrIter NULL_FacePtrIter = stupid_FacePtr_list.end();
-
-
-
-#ifdef USE_PARALLEL
-int numprocs;
-int my_id; 
-#endif
-
 char hostname[MAX_HOSTNAME]; 
-
+bool file_arg  = false;
 
 int main(int argc, char **argv)
 {
