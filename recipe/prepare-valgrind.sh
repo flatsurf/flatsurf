24b4b3797a494aa7ba64b849987b4254d07fdac1
Julian Rüth <julian.rueth@fsfe.org>
2019-01-27 21:00:46 +0100

    Use libc's checks
    since anaconda does not ship libasan for more fancy asan checks

--- recipe/prepare-valgrind.sh
+++ recipe/prepare-valgrind.sh
@@ -10,8 +10,8 @@ index 979e451..83b40cd 100644
  case `$CXX --version` in
      *GCC*|*gnu-c++*)
          export EXTRA_CXXFLAGS="-Werror -Wall -Wextra -pedantic -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-promo -Wstrict-null-sentinel -Wundef -fdiagnostics-show-option -Wconversion"
-+        # Instrument code to detect errors at runtime
-+        export EXTRA_CXXFLAGS="-fsanitize=address -fsanitize=leak -fsanitize=undefined -static-libasan -static-liblsan -static-libubsan"
++        # Ask libc to detect heap corruption
++        export EXTRA_CXXFLAGS="-lmcheck"
          ;;
      *clang*)
          export EXTRA_CXXFLAGS="-Werror -Weverything -Wno-padded -Wno-exit-time-destructors -Wno-undefined-func-template -Wno-global-constructors -Wno-c++98-compat -Wno-missing-prototypes"
@@ -22,7 +22,7 @@ diff --git a/test/run.sh b/test/run.sh
 index ae6f789..9a545a4 100755
 --- a/test/run.sh
 +++ b/test/run.sh
-@@ -7,6 +7,13 @@ set -exo pipefail
+@@ -7,6 +7,12 @@ set -exo pipefail
  # Go to the test directory, so the path names are correct
  cd -P -- "$(dirname -- "$0")"
  
@@ -32,7 +32,6 @@ index ae6f789..9a545a4 100755
 +valgrind --leak-check=full polygon 1 1 2 2 --depth=10 --follow_depth=50 --show_cyls --show_moduli --cyls_only --quiet
 +
 +# Run other tests with less expensive instrumentation
-+export ASAN_OPTIONS="detect_invalid_pointer_pairs=2"
  for output in *.sample.out; do
      diff <(polygon `basename "$output" .sample.out` | tail -n +2) <(tail -n +2 "$output");
  done
