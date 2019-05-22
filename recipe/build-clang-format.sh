ee719b1733cbfef89de426b02fa0c3678fe10fc6
Julian Rüth <julian.rueth@fsfe.org>
2019-05-23 01:49:49 +0200

    Add missing false_v definition

--- recipe/build-clang-format.sh
+++ recipe/build-clang-format.sh
@@ -3,5 +3,5 @@ set -e
 
 # Enforce proper formatting of our source code
 conda install -y --quiet clangdev=8.0.0
-clang-format -i -style=file `git ls-files '*.cc' '*.hpp' '*.ipp' '*.h' '*.h.in' '.hpp.in'`
+clang-format -i -style=file `git ls-files '*.cc' '*.hpp' '*.ipp' '*.h' '*.h.in' '*.hpp.in'`
 git diff --exit-code
