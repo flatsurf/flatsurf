0db89ffe9a4824969670f9e11d0b94dce022e5f1
Julian Rüth <julian.rueth@fsfe.org>
2019-08-12 12:04:04 +0200

    install pyflatsurf without CXXFLAGS

--- recipe/install-pyflatsurf.sh
+++ recipe/install-pyflatsurf.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd pyflatsurf
 
-# Run make install with the same flags that we use when invoking make
-source $RECIPE_DIR/cflags.sh
-
-make install CXXFLAGS="$CXXFLAGS $EXTRA_CXXFLAGS"
+make install
