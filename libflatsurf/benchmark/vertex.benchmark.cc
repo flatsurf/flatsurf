64edcdfbaae4b8a37c3e0c345b02e8cfb9425e21
Julian Rüth <julian.rueth@fsfe.org>
2020-02-12 13:40:12 +0100

    Add Vertex benchmark

---
+++ libflatsurf/benchmark/vertex.benchmark.cc
@@ -0,0 +1,57 @@
+/**********************************************************************
+ *  This file is part of flatsurf.
+ *
+ *        Copyright (C) 2019-2020 Vincent Delecroix
+ *        Copyright (C) 2019-2020 Julian Rüth
+ *
+ *  Flatsurf is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Flatsurf is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with flatsurf. If not, see <https://www.gnu.org/licenses/>.
+ *********************************************************************/
+
+#include <benchmark/benchmark.h>
+
+#include "../flatsurf/vertex.hpp"
+#include "../flatsurf/half_edge.hpp"
+
+#include "surfaces.hpp"
+
+using benchmark::DoNotOptimize;
+using benchmark::State;
+
+namespace flatsurf::benchmark {
+using namespace flatsurf::test;
+
+void VertexAtTarget(State& state) {
+  const auto surface = makeL<Vector<long long>>();
+  HalfEdge e(1);
+
+  for (auto _ : state) {
+    DoNotOptimize(Vertex::target(e, *surface));
+  }
+}
+BENCHMARK(VertexAtTarget);
+
+void VertexAtSource(State& state) {
+  const auto surface = makeL<Vector<long long>>();
+  HalfEdge e(1);
+
+  for (auto _ : state) {
+    DoNotOptimize(Vertex::source(e, *surface));
+  }
+}
+BENCHMARK(VertexAtSource);
+
+}  // namespace flatsurf::benchmark
+
+
+
