7220ad0340a5a9bd67c294dbc4cd2311ec0e33bf
Julian Rüth <julian.rueth@fsfe.org>
2020-10-05 02:35:17 +0200

    clang-format source code

--- libflatsurf/test/main.cc
+++ libflatsurf/test/main.cc
@@ -28,8 +28,8 @@
 #define CATCH_CONFIG_RUNNER
 #include "external/catch2/single_include/catch2/catch.hpp"
 
-int main( int argc, char* argv[] ) {
-  int result = Catch::Session().run( argc, argv );
+int main(int argc, char* argv[]) {
+  int result = Catch::Session().run(argc, argv);
 
   flint_cleanup();
 
