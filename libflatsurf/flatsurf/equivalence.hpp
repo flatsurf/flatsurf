b994b693d7b8afed9fab12af69d081559936b7ae
Julian Rüth <julian.rueth@fsfe.org>
2022-09-28 13:28:06 +0200

    Extend predefined Equivalence classes

--- libflatsurf/flatsurf/equivalence.hpp
+++ libflatsurf/flatsurf/equivalence.hpp
@@ -33,25 +33,74 @@ namespace flatsurf {
 template <typename Surface>
 class Equivalence {
   static_assert(std::is_same_v<Surface, std::decay_t<Surface>>, "type parameter must not have modifiers such as const");
+  using T = typename Surface::Coordinate;
 
  public:
-  template <typename T>
-  Equivalence(T&& impl) : self(impl) {}
-
-  // Return the combinatorial equivalence of surfaces.
-  // Two surfaces are combinatorially equivalent if their combinatorial
-  // structure is equal modulo relabeling of half edges, i.e., after relabeling
-  // they have the same triples of half edges describing their faces.
-  static Equivalence combinatorial();
+  template <typename I>
+  Equivalence(I&& impl) : self(impl) {}
 
   // Return the combinatorial equivalence of surfaces on marked edges.
   // The edges singled out by the predicate must be such that the graph they
   // form is connected and has no cut-edges. In other words, the surface is
   // such that we can walk the boundaries of the (not necessarily triangular)
-  // faces.
+  // faces; the default is to pick all edges.
   // Two surfaces are then equivalent if after relabeling they have the same
   // sequence of half edges along their faces.
-  static Equivalence combinatorial(std::function<bool(const Surface&, Edge)>);
+  static Equivalence combinatorial(std::function<bool(const Surface&, Edge)> = all);
+
+  // Return the equivalence of surfaces modulo labeling on marked edges.
+  // The edges singled out by the predicate must be such that the graph they
+  // form is connected and has no cut-edges. In other words, the surface is
+  // such that we can walk the boundaries of the (not necessarily triangular)
+  // faces; the default is to pick all edges.
+  // Two surfaces are then equivalent if half edges can be relabeled such that
+  // the selected faces are identical, i.e., they are combinatorially
+  // equivalent and also assign the same vector to each half edge label.
+  static Equivalence unlabeled(std::function<bool(const Surface&, Edge)> = all);
+
+  // Return the rotational equivalence of surfaces on marked edges.
+  // The edges singled out by the predicate must be such that the graph they
+  // form is connected and has no cut-edges. In other words, the surface is
+  // such that we can walk the boundaries of the (not necessarily triangular)
+  // faces; the default is to pick all edges.
+  // Two surfaces are then equivalent if the half edges can be relabeled such
+  // that the selected faces are identical module a fixed rotation.
+  static Equivalence rotational(std::function<bool(const Surface&, Edge)> = all);
+
+  // Return the SL2 equivalence of surfaces on marked edges.
+  // The edges singled out by the predicate must be such that the graph they
+  // form is connected and has no cut-edges. In other words, the surface is
+  // such that we can walk the boundaries of the (not necessarily triangular)
+  // faces; the default is to pick all edges.
+  // Two surfaces are then equivalent if the half edges can be relabeled such
+  // that the selected faces are identical module a fixed transformation in
+  // SL2(R) or SL2±(R) if oriented is not set.
+  static Equivalence areaPreserving(bool oriented, std::function<bool(const Surface&, Edge)> = all);
+
+  // Return the GL2 equivalence of surfaces on marked edges.
+  // The edges singled out by the predicate must be such that the graph they
+  // form is connected and has no cut-edges. In other words, the surface is
+  // such that we can walk the boundaries of the (not necessarily triangular)
+  // faces; the default is to pick all edges.
+  // Two surfaces are then equivalent if the half edges can be relabeled such
+  // that the selected faces are identical module a fixed transformation.
+  // Candidates for that transformation are produced by the provided
+  // normalization. Namely, given two half edges that are counterclockwise
+  // consecutive at a vertex, the normalization produces a linear
+  // transformation that brings these half edges into a normal form. If
+  // oriented is not set, then transformation with negative determinant are
+  // also considered, i.e., the half edges presented to the normalization are
+  // also clockwise consecutive at a vertex.
+  static Equivalence linear(bool oriented, std::function<std::tuple<T, T, T, T>(const Surface&, HalfEdge, HalfEdge)> normalization = orthonormalize, std::function<bool(const Surface&, Edge)> = all);
+
+  // Return a matrix [[a, b], [c, d]] that turns the half edges into (1, 0) and (0, 1), respectively.
+  static std::tuple<T, T, T, T> orthonormalize(const Surface&, HalfEdge, HalfEdge);
+
+  // Return true for any input.
+  static bool all(const Surface&, Edge);
+
+  // Return true iff the edge forms the boundary of a Delaunay cell.
+  static bool delaunayCell(const Surface&, Edge);
 
   template <typename S>
   friend std::ostream &operator<<(std::ostream &, const Equivalence<S> &);
