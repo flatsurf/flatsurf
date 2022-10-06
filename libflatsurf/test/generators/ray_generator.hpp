654b45b47bbc41336773c728e5520380ff2e3ba7
Julian Rüth <julian.rueth@fsfe.org>
2022-10-06 14:31:18 +0200

    Implement ray generator for testing

--- libflatsurf/test/generators/ray_generator.hpp
+++ libflatsurf/test/generators/ray_generator.hpp
@@ -46,19 +46,39 @@ class RayGenerator : public Catch::Generators::IGenerator<Ray<FlatTriangulation<
   
   Ray<FlatTriangulation<T>> current;
 
-  Ray<FlatTriangulation<T>> make(RAY kind) {
+  std::optional<Ray<FlatTriangulation<T>>> make(RAY kind) {
     switch (kind) {
       case RAY::VERTEX_ALONG_EDGE:
+        return Ray{*surface, face};
       case RAY::EDGE_ALONG_EDGE:
+        {
+          if constexpr (!hasFractions<T>)
+            return std::nullopt;
+          const auto v = surface->fromHalfEdge(face) / 2;
+          return Ray{Point{*surface, face, v}, v};
+        }
       case RAY::EDGE_TO_FACE:
+        {
+          if constexpr (!hasFractions<T>)
+            return std::nullopt;
+          const auto p = surface->fromHalfEdge(face) / 2;
+          const auto v = Vector<T>{p.y(), -p.x()};
+          return Ray{Point{*surface, face, p}, v};
+        }
       case RAY::FACE_TO_FACE:
+        {
+          if constexpr (!hasFractions<T>)
+            return std::nullopt;
+          const auto v = surface->fromHalfEdge(face) / 8 + surface->fromHalfEdge(surface->nextAtVertex(face)) / 8;
+          return Ray{Point(*surface, face, v), v};
+        }
       default:
         throw std::logic_error("not implemented: RayGenerator::make()");
     }
   }
 
  public:
-  RayGenerator(std::shared_ptr<const FlatTriangulation<T>> surface, HalfEdge face) : surface(surface), face(face), state(RAY::VERTEX_ALONG_EDGE), current(make(state)) {
+  RayGenerator(std::shared_ptr<const FlatTriangulation<T>> surface, HalfEdge face) : surface(surface), face(face), state(RAY::VERTEX_ALONG_EDGE), current(*make(state)) {
   }
 
   const Ray<FlatTriangulation<T>>& get() const override {
@@ -66,14 +86,17 @@ class RayGenerator : public Catch::Generators::IGenerator<Ray<FlatTriangulation<
   }
 
   bool next() override {
-    if (state == RAY::LAST)
-      return false;
+    while (state != RAY::LAST) {
+      state = static_cast<RAY>(static_cast<int>(state) + 1);
 
-    state = static_cast<RAY>(static_cast<int>(state) + 1);
+      const auto ray = make(state);
+      if (ray) {
+        current = *ray;
+        return true;
+      }
+    }
 
-    current = make(state);
-
-    return true;
+    return false;
   }
 };
 
