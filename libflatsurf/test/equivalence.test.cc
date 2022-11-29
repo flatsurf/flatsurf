c61fc8fac6171ec90c6a1703d3a88ccade28889a
Julian Rüth <julian.rueth@fsfe.org>
2022-11-29 16:38:25 +0200

    Add tests for combinatorial codes

--- libflatsurf/test/equivalence.test.cc
+++ libflatsurf/test/equivalence.test.cc
@@ -20,22 +20,73 @@
 #include "../flatsurf/edge.hpp"
 #include "../flatsurf/equivalence.hpp"
 #include "../flatsurf/equivalence_class.hpp"
+#include "../flatsurf/deformation.hpp"
+#include "../flatsurf/point.hpp"
 #include "generators/surface_generator.hpp"
+#include "generators/half_edge_generator.hpp"
 
 namespace flatsurf::test {
 
 TEMPLATE_TEST_CASE("Combinatorial Equivalence of Surfaces", "[Equivalence][combinatorial]", (long long), (mpz_class), (mpq_class), (renf_elem_class), (exactreal::Element<exactreal::IntegerRing>), (exactreal::Element<exactreal::RationalField>), (exactreal::Element<exactreal::NumberField>)) {
   using T = TestType;
+  using Surface = FlatTriangulation<T>;
 
   const auto surface = GENERATE_SURFACES(T);
   CAPTURE(surface);
 
   SECTION("A Surface is Combinatorially Equivalent to Itself") {
-    const auto equivalence = Equivalence<FlatTriangulation<T>>::combinatorial();
+    const auto equivalence = Equivalence<Surface>::combinatorial();
 
     REQUIRE(EquivalenceClass(*surface, equivalence) == EquivalenceClass(*surface, equivalence));
   }
-}
 
+  SECTION("Equivalence modulo Flipping an Edge") {
+    const auto halfEdge = GENERATE_COPY(halfEdges(surface));
+
+    // Don't test for each edge twice.
+    if (halfEdge == halfEdge.edge().positive()) {
+      if (surface->convex(halfEdge, true)) {
+        auto flipped = surface->clone();
+        flipped.flip(halfEdge);
+        CAPTURE(flipped);
+
+        const auto equivalence = Equivalence<Surface>::combinatorial([&](const Surface&, const Edge edge) { return edge != halfEdge; });
+
+        // If we ignore the flipped edge, the surfaces must be indistinguishable.
+        REQUIRE(EquivalenceClass(*surface, equivalence) == EquivalenceClass(flipped, equivalence));
+      }
+    }
+  }
+
+  SECTION("Equivalence modulo Linear Deformation") {
+    const auto equivalence = Equivalence<Surface>::combinatorial();
+
+    // Linear Deformation do not change the combinatorial structure (if their
+    // determinant is positive.)
+    REQUIRE(EquivalenceClass(*surface, equivalence) == EquivalenceClass(surface->applyMatrix(T(2), T(1), T(3), T(4)).codomain(), equivalence));
+  }
+
+  if constexpr (hasFractions<T>) {
+    SECTION("Equivalence modulo Insertions") {
+      const Point<Surface> point{*surface, HalfEdge(1), T(1), T(1), T(1)};
+
+      const auto inserted = surface->insert(point);
+
+      SECTION("Inserting a Point Changes the Combinatorial Structure of a Surface") {
+        const auto equivalence = Equivalence<Surface>::combinatorial();
+
+        REQUIRE(EquivalenceClass(*surface, equivalence) != EquivalenceClass(inserted.codomain(), equivalence));
+      }
+
+      SECTION("Inserting a Point does not Change the Combinatorial Structure away from that Point") {
+        const auto equivalence = Equivalence<Surface>::combinatorial([&](const Surface&, const Edge edge) {
+          return edge.index() < surface->size();
+        });
+
+        REQUIRE(EquivalenceClass(*surface, equivalence) == EquivalenceClass(inserted.codomain(), equivalence));
+      }
+    }
+  }
+}
 
 }
